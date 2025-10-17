d91a2338b3d85e32cff9be1a94e521a5f5a214f0
HUAWEI <ognevanikol81@yandex.ru>
2025-10-17 15:03:31 +0300

    Удален класс Main

--- src/geometry/Main.java
+++
@@ -1,9 +0,0 @@
-package geometry;
-import javax.swing.SwingUtilities;
-
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(GeometryMainFrame::new);
-    }
-}
